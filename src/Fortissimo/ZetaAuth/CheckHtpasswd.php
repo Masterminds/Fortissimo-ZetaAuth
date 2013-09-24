e412a9deb5e761e7d28d0f16dce128c2e9b0963c
Matt Butcher <technosophos@gmail.com>
2013-09-24 15:30:09 -0600

    Re-org to src directory.

---
+++ src/Fortissimo/ZetaAuth/CheckHtpasswd.php
@@ -0,0 +1,38 @@
+<?php
+namespace Fortissimo\ZetaAuth;
+
+/**
+ * ZetaCompoents authentication via Fortissimo.
+ */
+class CheckHtpasswd extends \Fortissimo\Command\Base {
+  public function expects() {
+    return $this
+      ->description("Provide authentication services via ZetaComponents.")
+      ->usesParam("user", "User name")
+      ->usesParam("password", "Password")
+      ->usesParam("htpasswd", "Full path to an htpasswd file.")
+      ->andReturns("The user name.")
+      ;
+  }
+
+  public function doCommand() {
+
+    $session = new ezcAuthenticationSession();
+    $session->start();
+
+    $user = $this->param("user", $session->load());
+    $pass = $this->param("password");
+
+    $credentials = ezcAuthenticationPasswordCredentials($user, $pass);
+    $authentication = new ezcAuthentication($credentials);
+    $authentication->session = $session;
+    $authentication->addFilter( new ezcAuthenticationHtpasswdFilter($pwfile));
+
+    if (!$authentication->run()) {
+      throw new \Fortissimo\FatalException("Failed auth.");
+    }
+
+    return $user;
+  }
+
+}
